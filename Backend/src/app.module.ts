6bc4d9d5228aea57e927eb27ece0a90209e6cb60
Pablo Navarro <paakitto91@gmail.com>
2026-09-09 00:18:44 -0300

    chore(backend): configure environment and database

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -4,12 +4,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+
+      validationSchema: Joi.object({ //en esta sección se definen las variables de entorno que se van a utilizar en la aplicación y se validan con Joi  
+        NODE_ENV: Joi.string()
+        .valid('development', 'test', 'production')
+        .default('development'),
+
+      PORT: Joi.number().port().default(3000),
+
+      DB_HOST: Joi.string().required(),
+      DB_PORT: Joi.number().port().required(),
+      DB_USERNAME: Joi.string().required(),
+      DB_PASSWORD: Joi.string().required(),
+    DB_DATABASE: Joi.string().required(),
     }),
+  }),
 
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
